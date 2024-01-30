4dd34958530e9e660487aaa0b9319ef75c20de5b
Jorge Luis Escobedo Ocaña <jescobedooc@unsa.edu.pe>
2024-01-30 01:24:36 -0500

    interfaz avanzada

--- ASISTENTE.py
+++ ASISTENTE.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import threading
 import queue
+import json
 
 recognizer = sr.Recognizer()
 microphone = sr.Microphone()
@@ -15,25 +16,30 @@ root.geometry("800x600")
 root.title("ASISTENTE VIRTUAL")
 
 # Initialize images/widgets globally
+image_queue = queue.Queue()
+
 image = Image.open("IMG/iaBackground.jpg")
 image = image.resize((790, 450))
-img = ImageTk.PhotoImage(image)
+photo = ImageTk.PhotoImage(image)
+image_label = tk.Label(root, image = photo)
+image_label.grid(column=0, row=0, pady=20)
+image_queue.put(photo)
 
-lbl_image = tk.Label(root, image=img)
-lbl_image.grid(column=0, row=0, pady=20)
-
-lbl_text = tk.Label(root, text="Haz click en el boton 'iniciar' para empezar", font=("Arial", 16, "bold"))
+lbl_text = tk.Label(root, text="Haz click en el boton 'iniciar' para empezar", font=("Arial", 13, "bold"))
 lbl_text.grid(column=0, row=1)
 
 # Queue for communication between threads
 queue_ui_to_main = queue.Queue()
 queue_main_to_ui = queue.Queue()
 
+with open('basedatos.json', 'r') as archivo:
+    datos = json.load(archivo)
+
 def texto_a_audio(text):
     engine.say(text)
     engine.runAndWait()
 
-def capturar_voz(reconocer, microfono, tiempo_ruido=0.7):
+def capturar_voz(reconocer, microfono, tiempo_ruido=0.5):
     with microfono as fuente:
         reconocer.adjust_for_ambient_noise(fuente, duration=tiempo_ruido)
         print("Escuchando...")
@@ -59,22 +65,80 @@ def main_thread_logic():
         if command == "start":
             execute_start_logic()
 
+# def enviar_image(ruta):
+#    image = Image.open("" + ruta)
+#    image = image.resize((790, 450))
+#    photo = ImageTk.PhotoImage(image)
+#    image_queue.put(photo)
+
 def execute_start_logic():
     send_text_to_ui("Bienvenid@")
     texto_a_audio("Bienvenido")
-    send_text_to_ui("Tu nombre: ")
-    nombre = enviar_voz()
+    send_text_to_ui("¿Comó te llamas?")
+    texto_a_audio("¿Comó te llamas?")
+    nombre = "w"
     send_text_to_ui("Hola " + nombre)
     texto_a_audio("Hola {}. Mucho gusto.".format(nombre))
+    texto_a_audio(datos["bienvenida"])
     texto_a_audio(
-        "{} Ahora voy a explicarte sobre las opciones que tiene este programa. Tienes 3 opciones para escoger.".format(
+        "{} ahora voy a explicarte sobre las opciones que tiene este programa. Tienes 3 opciones para escoger.".format(
             nombre))
     send_text_to_ui("1) Aprendizaje   2) Tests    3) Juegos")
     texto_a_audio("Aprendizaje. Tests. Juegos.")
     texto_a_audio(
         "La opción Aprendizaje es donde podrás aprender todo con respecto a Programación. La opción Tests es donde podrás poner en práctica lo que aprendiste mediante preguntas. Y por último, la tercer opción, es Juegos, donde también podrás poner en acción lo que aprendiste jugando.")
     send_text_to_ui("¿Qué opción eliges?")
-    texto_a_audio("¿Aprendizaje? ¿Tests? ¿Juegos?")
+      
+    respuesta = "aprendizaje"
+
+    if respuesta == "aprendizaje":
+
+        #enviar_image("IMG/computador.jpg")
+        image = Image.open("IMG/computador.jpg")
+        image = image.resize((790, 450))
+        photo = ImageTk.PhotoImage(image)
+        image_queue.put(photo)
+
+        send_text_to_ui("Elegiste la opcion APRENDIZAJE.")
+        texto_a_audio("Elegiste la opcion APRENDIZAJE.")
+        texto_a_audio("Muy bien antes de empezar quisiera hacer una introduccion a el tema conocido como Programación.")
+        send_text_to_ui("Definicion de PROGRAMACION.")
+        texto_a_audio(datos['definicion'])
+        send_text_to_ui("IMPORTANCIA.")
+        texto_a_audio(datos['importancia'])
+        send_text_to_ui("RAZONES.")
+        texto_a_audio(datos['razones'])
+        texto_a_audio("Como se puede apreciar en la imagen, la Programación contiene varios aspectos y caracteristicas importantes, tales como:")
+        texto_a_audio("Variables. Constantes. Tipos de datos. Operadores. Estructuras condicionales. Bucles. Funciones. Compiladores. Depuración. Algoritmos. Estructuras de datos. Programación Orientada a Objetos (POO).")
+        
+        respuesta = "estructuras de datos"
+
+        if respuesta == "variables":
+            texto_a_audio(datos['variables'])
+        elif respuesta == "constantes":
+            texto_a_audio(datos['constantes'])
+        elif respuesta == "tipos de datos":
+            texto_a_audio(datos['tipos_de_datos'])
+        elif respuesta == "operadores":
+            texto_a_audio(datos['operadores'])
+        elif respuesta == "estructuras condicionales":
+            texto_a_audio(datos['estructuras_condicionales'])
+        elif respuesta == "bucles":
+            texto_a_audio(datos['bucles'])
+        elif respuesta == "funciones":
+            texto_a_audio(datos['funciones'])
+        elif respuesta == "compiladores":
+            texto_a_audio(datos['compiladores'])
+        elif respuesta == "depuracion":
+            texto_a_audio(datos['depuracion'])
+        elif respuesta == "algoritmos":
+            texto_a_audio(datos['algoritmos'])
+        elif respuesta == "estructuras de datos":
+            texto_a_audio(datos['estructuras_de_datos']['definicion'])
+            texto_a_audio("existen distintos tipos de estructura, aqui te mostrare 5 de ellas.")
+            send_text_to_ui("1) Arrays 2) Listas enlazadas 3) Pilas 4) Colas 5) Hash\n¿Por cual deseas empezar?")
+            texto_a_audio("¿Por cual deseas empezar?")
+        
 
 def enviar_voz():
     palabra = capturar_voz(recognizer, microphone)
@@ -89,11 +153,23 @@ def send_text_to_ui(text):
     root.after(0, update_ui)
 
 def update_ui():
+    
     try:
-        text = queue_main_to_ui.get_nowait()
+        text = queue_main_to_ui.get_nowait() 
         lbl_text.config(text=text)
+        
     except queue.Empty:
-        root.after(100, update_ui)
+        pass
+    
+    try:
+        photo = image_queue.get_nowait()     
+        image_label.config(image = photo)        
+
+
+    except queue.Empty: 
+        pass
+        
+    root.after(100, update_ui)
 
 def start():
     queue_ui_to_main.put("start")
