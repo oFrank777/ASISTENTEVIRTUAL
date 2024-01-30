4c0b09ac79cb5ae2293e3a62f713cc2a317706a3
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2024-01-30 16:46:53 -0500

    Se ha modificado la interfaz, para que ya no de tirones en la ejecucion, se ha copiado el codigo de la rama prueba

--- ASISTENTE.py
+++ ASISTENTE.py
@@ -1,31 +1,47 @@
 import speech_recognition as sr
 import pyttsx3
-import time
-import sys
 import tkinter as tk
-import random
-import json
-from tkinter import messagebox
 from PIL import Image, ImageTk
+import threading
+import queue
+import json
 
 recognizer = sr.Recognizer()
 microphone = sr.Microphone()
-salir = False
+engine = pyttsx3.init()
 
-def texto_a_audio(comando):
-    palabra = pyttsx3.init()
-    palabra.say(comando)
-    palabra.runAndWait()
+root = tk.Tk()
+root.iconbitmap("IMG/icon.ico")
+root.geometry("800x600")
+root.title("ASISTENTE VIRTUAL")
 
-def capturar_voz(reconocer, microfono, tiempo_ruido = 0.7):
-    if not isinstance(reconocer, sr.Recognizer):
-        raise TypeError("'reconocer' no es de la instacia 'Recognizer'")
+# Initialize images/widgets globally
+image_queue = queue.Queue()
 
-    if not isinstance(microfono, sr.Microphone):
-        raise TypeError("'reconocer' no es de la instacia 'Recognizer'")
-    
+image = Image.open("IMG/iaBackground.jpg")
+image = image.resize((790, 450))
+photo = ImageTk.PhotoImage(image)
+image_label = tk.Label(root, image = photo)
+image_label.grid(column=0, row=0, pady=20)
+image_queue.put(photo)
+
+lbl_text = tk.Label(root, text="Haz click en el boton 'iniciar' para empezar", font=("Arial", 13, "bold"))
+lbl_text.grid(column=0, row=1)
+
+# Queue for communication between threads
+queue_ui_to_main = queue.Queue()
+queue_main_to_ui = queue.Queue()
+
+with open('basedatos.json', 'r') as archivo:
+    datos = json.load(archivo)
+
+def texto_a_audio(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def capturar_voz(reconocer, microfono, tiempo_ruido=0.5):
     with microfono as fuente:
-        reconocer.adjust_for_ambient_noise(fuente, duration = tiempo_ruido)
+        reconocer.adjust_for_ambient_noise(fuente, duration=tiempo_ruido)
         print("Escuchando...")
         audio = reconocer.listen(fuente)
 
@@ -40,76 +56,236 @@ def capturar_voz(reconocer, microfono, tiempo_ruido = 0.7):
         respuesta["suceso"] = False
         respuesta["error"] = "API no disponible"
     except sr.UnknownValueError:
-        respuesta["error"] = "Habla inteligible"
+        respuesta["error"] = "Habla ininteligible"
     return respuesta
 
+def main_thread_logic():
+    while True:
+        command = queue_ui_to_main.get()
+        if command == "start":
+            execute_start_logic()
+
+def execute_start_logic():
+    send_text_to_ui("Bienvenid@")
+    texto_a_audio("Bienvenido")
+    send_text_to_ui("¿Comó te llamas?")
+    texto_a_audio("¿Comó te llamas?")
+    nombre = enviar_voz()
+    send_text_to_ui("Hola " + nombre)
+    texto_a_audio("Hola {}. Mucho gusto.".format(nombre))
+    texto_a_audio(datos["bienvenida"])
+    texto_a_audio(
+        "{} ahora voy a explicarte sobre las opciones que tiene este programa. Tienes 3 opciones para escoger.".format(
+            nombre))
+    send_text_to_ui("1) Aprendizaje   2) Tests    3) Juegos")
+    texto_a_audio("Aprendizaje. Tests. Juegos.")
+    texto_a_audio(
+        "La opción Aprendizaje es donde podrás aprender todo con respecto a Programación. La opción Tests es donde podrás poner en práctica lo que aprendiste mediante preguntas. Y por último, la tercer opción, es Juegos, donde también podrás poner en acción lo que aprendiste jugando.")
+    send_text_to_ui("¿Qué opción eliges?")
+      
+    respuesta = enviar_voz()
+
+    if respuesta == "aprendizaje":
+
+        image = Image.open("IMG/introduccionProgramacion.png")
+        image = image.resize((790, 450))
+        photo = ImageTk.PhotoImage(image)
+        image_queue.put(photo)
+
+        send_text_to_ui("Elegiste la opcion APRENDIZAJE.")
+        texto_a_audio("Elegiste la opcion APRENDIZAJE.")
+        texto_a_audio("Muy bien antes de empezar quisiera hacer una introduccion a el tema conocido como Programación.")
+        send_text_to_ui("Definicion de PROGRAMACION.")
+        texto_a_audio(datos['definicion'])
+        send_text_to_ui("IMPORTANCIA.")
+        texto_a_audio(datos['importancia'])
+        send_text_to_ui("RAZONES.")
+        texto_a_audio(datos['razones'])
+        texto_a_audio("Como se puede apreciar en la imagen, la Programación contiene varios aspectos y caracteristicas importantes, tales como:")
+        send_text_to_ui("Conceptos de Programacion.")
+        texto_a_audio("Variables. Constantes. Tipos de datos. Operadores. Estructuras condicionales. Bucles. Funciones. Compiladores. Depuración. Algoritmos. Estructuras de datos. Programación Orientada a Objetos (POO).")
+        
+        respuesta = "estructuras de datos"
+
+        if respuesta == "variables":
+            texto_a_audio(datos['variables'])
+        elif respuesta == "constantes":
+            texto_a_audio(datos['constantes'])
+        elif respuesta == "tipos de datos":
+            texto_a_audio(datos['tipos_de_datos'])
+        elif respuesta == "operadores":
+            texto_a_audio(datos['operadores'])
+        elif respuesta == "estructuras condicionales":
+            texto_a_audio(datos['estructuras_condicionales'])
+        elif respuesta == "bucles":
+            texto_a_audio(datos['bucles'])
+        elif respuesta == "funciones":
+            texto_a_audio(datos['funciones'])
+        elif respuesta == "compiladores":
+            texto_a_audio(datos['compiladores'])
+        elif respuesta == "depuracion":
+            texto_a_audio(datos['depuracion'])
+        elif respuesta == "algoritmos":
+            texto_a_audio(datos['algoritmos'])
+        elif respuesta == "estructuras de datos":
+            
+            send_text_to_ui("Estructuras de datos:")
+            texto_a_audio(datos['estructuras_de_datos']['definicion'])
+            texto_a_audio("existen distintos tipos de estructura, aqui te mostrare 5 de ellas.")
+            send_text_to_ui("1) Arrays 2) Listas enlazadas 3) Pilas 4) Colas 5) Hash\n¿Por cual deseas empezar?")
+            texto_a_audio("¿Por cual deseas empezar?")
+            
+            while True:
+
+                respuesta = enviar_voz()
+
+                if cond(respuesta) == True:
+                    break
+
+                if respuesta == "arrays":
+                    send_text_to_ui("Arrays:")
+                    texto_a_audio(datos['estructuras_de_datos']['arrays']['descripcion'])
+                    texto_a_audio("aqui te presentamos ventajas y desventajas de este tipo de estructura. Ventajas:")
+                    texto_a_audio(datos['estructuras_de_datos']['arrays']['ventajas'])
+                    texto_a_audio("Desventajas:")
+                    texto_a_audio(datos['estructuras_de_datos']['arrays']['desventajas'])
+                
+                elif respuesta == "listas enlazadas":
+                    send_text_to_ui("Lista enlazadas:")
+                    texto_a_audio(datos['estructuras_de_datos']['listas_enlazadas']['descripcion'])
+                    texto_a_audio("aqui te presentamos ventajas y desventajas de este tipo de estructura. Ventajas:")
+                    texto_a_audio(datos['estructuras_de_datos']['listas_enlazadas']['ventajas'])
+                    texto_a_audio("Desventajas:")
+                    texto_a_audio(datos['estructuras_de_datos']['listas_enlazadas']['desventajas'])
+                
+                elif respuesta == "pilas":
+                    send_text_to_ui("Pilas:")
+                    texto_a_audio(datos['estructuras_de_datos']['pilas']['descripcion'])
+                    texto_a_audio("aqui te presentamos ventajas y desventajas de este tipo de estructura. Ventajas:")
+                    texto_a_audio(datos['estructuras_de_datos']['pilas']['ventajas'])
+                    texto_a_audio("Desventajas:")
+                    texto_a_audio(datos['estructuras_de_datos']['pilas']['desventajas'])
+                    
+                elif respuesta == "colas":
+                    send_text_to_ui("Colas:")
+                    texto_a_audio(datos['estructuras_de_datos']['colas']['descripcion'])
+                    texto_a_audio("aqui te presentamos ventajas y desventajas de este tipo de estructura. Ventajas:")
+                    texto_a_audio(datos['estructuras_de_datos']['colas']['ventajas'])
+                    texto_a_audio("Desventajas:")
+                    texto_a_audio(datos['estructuras_de_datos']['colas']['desventajas'])
+
+                elif respuesta == "hash":
+                    send_text_to_ui("Hash:")
+                    texto_a_audio(datos['estructuras_de_datos']['hash']['descripcion'])
+                    texto_a_audio("aqui te presentamos ventajas y desventajas de este tipo de estructura. Ventajas:")
+                    texto_a_audio(datos['estructuras_de_datos']['hash']['ventajas'])
+                    texto_a_audio("Desventajas:")
+                    texto_a_audio(datos['estructuras_de_datos']['hash']['desventajas'])
+
+                send_text_to_ui("¿Deseas saber sobre otra estructura?\n1) Arrays 2) Listas enlazadas 3) Pilas 4) Colas 5) Hash 6) No")
+                texto_a_audio("¿Deseas saber sobre otra estructura?")
+
+        elif respuesta == "progrmacion orientada a objetos":
+            send_text_to_ui("Programacion Orientada a Objetos:")
+            texto_a_audio(datos['POO']['definicion'])
+            texto_a_audio("existen distintos conceptos importantes en Programacion Orientada a objetos, aqui te mostrare 5 de ellas.")
+            send_text_to_ui("1) Clases 2) Objetos 3) Herencia 4) Polimorfismo 5) Encapsulamiento\n¿Por cual deseas empezar?")
+            texto_a_audio("¿Por cual deseas empezar?")
+
+            while True:
+
+                respuesta = enviar_voz()
+
+                if cond(respuesta) == True:
+                    break
+
+                if respuesta == "clases":
+                    send_text_to_ui("Clases:")
+                    texto_a_audio(datos['POO']['clases']['definicion'])
+                    texto_a_audio("Observa el ejemplo")
+                    texto_a_audio(datos['POO']['arrays']['ejemplo'])
+                                    
+                elif respuesta == "objetos":
+                    send_text_to_ui("Objetos:")
+                    texto_a_audio(datos['POO']['objetos']['definicion'])
+                    texto_a_audio("Observa el ejemplo")
+                    texto_a_audio(datos['POO']['objetos']['ejemplo'])
+                
+                elif respuesta == "herencia":
+                    send_text_to_ui("Herencia:")
+                    texto_a_audio(datos['POO']['herencia']['definicion'])
+                    texto_a_audio("Observa el ejemplo")
+                    texto_a_audio(datos['POO']['herencia']['ejemplo'])
+                                        
+                elif respuesta == "polimorfismo":
+                    send_text_to_ui("Polimorfismo:")
+                    texto_a_audio(datos['POO']['polimorfismo']['definicion'])
+                    texto_a_audio("Observa el ejemplo")
+                    texto_a_audio(datos['POO']['polimorfismo']['ejemplo'])
+                
+                elif respuesta == "encapsulamiento":
+                    send_text_to_ui("Encapsulamiento:")
+                    texto_a_audio(datos['POO']['encapsulamiento']['definicion'])
+                    texto_a_audio("Observa el ejemplo")
+                    texto_a_audio(datos['POO']['encapsulamiento']['ejemplo'])
+
+                send_text_to_ui("¿Deseas saber sobre otro concepto?\n1) Clases 2) Objetos 3) Herencia 4) Polimorfismo 5) Encapsulamiento 6) No")
+                texto_a_audio("¿Deseas saber sobre otra estructura?")
+
+    elif respuesta == "tests":
+        print ("Test")
+    elif respuesta == "juegos":
+        print ("juegos")
+    else:
+        print("no elegiste nada")
+
+def cond(opcion):
+                if opcion == "no":
+                    return True
+                else:
+                    return False
+                
 def enviar_voz():
-    while (1):
-        palabra = capturar_voz(recognizer, microphone)
-        if palabra["mensaje"]:
-            break
-        if not palabra["suceso"]:
-            print("Algo no está bien. No puedo reconocer tu micrófono o no lo tienes enchufado. <", nombre["error"],">")
-            texto_a_audio("Algo no está bien. No puedo reconocer tu micrófono o no lo tienes enchufado.")
-            exit(1)
-        print("No pude escucharte, ¿podrias repetirlo?\n")
-        texto_a_audio("No pude escucharte, ¿podrias repetirlo?")
+    palabra = capturar_voz(recognizer, microphone)
+    if not palabra["suceso"]:
+        print("Algo no está bien. No puedo reconocer tu micrófono o no lo tienes enchufado. <", palabra["error"], ">")
+        texto_a_audio("Algo no está bien. No puedo reconocer tu micrófono o no lo tienes enchufado.")
+        exit(1)
     return palabra["mensaje"].lower()
 
-with open('basedatos.json', 'r') as archivo:
-    datos = json.load(archivo)
+def send_text_to_ui(text):
+    queue_main_to_ui.put(text)
+    root.after(0, update_ui)
 
-if __name__ == "__main__":
-
-    root = tk.Tk()
-    root.iconbitmap("IMG/icon.ico") 
-
-    root.geometry("800x600") 
-    root.title("ASISTENTE VIRTUAL")
-
-    # Espacio para imagen 
-    image = Image.open("IMG/iaBackground.jpg")
-    image = image.resize((790, 450))
-    img = ImageTk.PhotoImage(image)
-
-    lbl_image = tk.Label(root, image=img)
-    lbl_image.grid(column=0, row=0, pady=20)  # Añadido pady para espacio vertical
-
-    text = "Haz click en el boton 'iniciar' para empezar"
-
-    lbl_text = tk.Label(root, text=text, font=("Arial", 16, "bold"))
-    lbl_text.grid(column=0, row=1)
-
-
-    # Botón inicio
-    def start():
-        print("running assistant ... :V")
+def update_ui():
+    
+    try:
+        text = queue_main_to_ui.get_nowait() 
+        lbl_text.config(text=text)
         
-        #USANDO LA FUNCION TEXTO_A_AUDIO SE HACE LEER CADENAS DE TEXTO, COMO SI LA COMPUTADORA TE ESTUVIERA HABLANDO
-        lbl_text.config(text="Bienvenid@")
-
-        texto_a_audio(datos['bienvenida'])
-        
-        lbl_text.config(text="Tu nombre: ")
-        
-        nombre = enviar_voz()
-        
-        lbl_text.config(text="Hola "+nombre)
-        texto_a_audio("Hola {}. Mucho gusto.".format(nombre))
-        texto_a_audio("{} Ahora voy a explicarte sobre las opciones que tiene este programa. Tienes 3 opciones para escoger.".format(nombre))
-        lbl_text.config(text="1) Aprendizaje   2) Tests    3) Juegos")
-        texto_a_audio("Aprendizaje. Tests. Juegos.")
-        texto_a_audio("La opción Aprendizaje es donde podrás aprender todo con respecto a Programacion. La opción Tests es donde podrás poner en práctica lo que aprendiste mediante preguntas. Y por último, la tercer opción, es Juegos, donde tambien podrás poner en accion lo que aprendiste jugando.")
-        texto_a_audio("¿Qué opción eliges?")
-        time.sleep(0.5)
-        texto_a_audio("¿Aprendizaje? ¿Tests? ¿Juegos?")
-        
-        #SE USA LA FUNCION SLEEP DE LA LIBRERIA TIME PARA PAUSAR UN TIEMPO LA EJECUCION DEL PROGRAMA
-        #PARA QUE LA INTERACCION SEA MAS NATURAL
-        time.sleep(0.5)
+    except queue.Empty:
+        pass
+    
+    try:
+        photo = image_queue.get_nowait()     
+        image_label.config(image = photo)        
 
 
-    btn_start = tk.Button(root, text="Iniciar", command=start, font=("Arial", 12, "bold"))  # Cambiado el tamaño de la fuente
-    btn_start.grid(column=0, row=2, pady=10)  # Añadido pady para espacio vertical
+    except queue.Empty: 
+        pass
+        
+    root.after(100, update_ui)
 
-    root.mainloop()
+def start():
+    queue_ui_to_main.put("start")
+
+# Start the main thread
+main_thread = threading.Thread(target=main_thread_logic)
+main_thread.daemon = True
+main_thread.start()
+
+btn_start = tk.Button(root, text="Iniciar", command=start, font=("Arial", 12, "bold"))
+btn_start.grid(column=0, row=2, pady=10)
+
+# Run the main loop directly
+root.mainloop()
